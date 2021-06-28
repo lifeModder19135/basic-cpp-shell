644f5882c85f587cfb33792b63891805eda425d0
Nathan <Tolbert>
2021-06-28 04:30:22 -0400

    init

---
+++ cppshell.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
+
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+#define clear printf("\033[H\033[J") 
+
+
+getenv();
+
+
+
+
