cf2298926dd51ed26efe6ca5a146b78ea89d2fee
Nathan <Tolbert>
2021-07-03 18:25:23 -0400

    added function to take input &write it to history.

--- cppshell.cpp
+++ cppshell.cpp
@@ -11,10 +11,32 @@
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
 #define clear printf("\033[H\033[J") 
+void initShell()
+{
+    printf("\n\n\n\n**************************************");
+    printf("\n\n\n\n**************************************");
+    printf("\n\n\n\n**********  HELLO WOOORLD!  **********");
+    printf("\n\n\n\n**************************************");    
+    char* user = getenv("USER");
+    printf("\n\n\n\n**************************************");
+    printf("\n\n\n\n**************************************");
+    sleep(1);
+    clear();
+}
 
+// take user input from command line
+int takeInput(char* str)
+{
+    char* buffer;
 
-getenv();
-
-
+    buffer = readline("\n$$$ ");
+    if(strlen(buffer) != 0){
+        add_history(buffer);
+        strcpy(str, buffer);
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
 
