54fc7cfb92aeddfcadc56e06dce706754cabbdcc
Nathan Tolbert <ntolbertu85@gmail.com>
2021-07-05 00:24:39 -0400

    07052021 -- Added parseSpace() function

--- cppshell.cpp
+++ cppshell.cpp
@@ -179,5 +179,13 @@ parsePipe(char* str, char** )
 
 void parseSpace(char* str, char** parsed) 
 {
-    
+    int i; 
+       
+    for (i = 0, i < MAXLIST, i++) {
+        parsed[i] = strsep(&str, " ");         
+        if (parsed[i] == NULL)
+            break;
+        if (strlen(parsed[i]) == 0)
+            i--;
+    }
 }
